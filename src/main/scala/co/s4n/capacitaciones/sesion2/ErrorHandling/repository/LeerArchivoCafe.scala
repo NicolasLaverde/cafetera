230c2e911cc72a2caf0c6415b6c3889a720d7c0f
Daniel <danielbernal@seen4n.com>
2017-03-08 10:29:30 -0500

    genéricos en lectura y escritura de archivos

--- src/main/scala/co/s4n/capacitaciones/sesion2/ErrorHandling/repository/LeerArchivoCafe.scala
+++ src/main/scala/co/s4n/capacitaciones/sesion2/ErrorHandling/repository/LeerArchivoCafe.scala
@@ -2,16 +2,19 @@ package co.s4n.capacitaciones.sesion2.ErrorHandling.repository
 
 import java.io.{ BufferedWriter, File, FileWriter, InputStream }
 
-import co.s4n.capacitaciones.sesion2.ErrorHandling.{ CafeGrano, Ingrediente, Leche }
+import co.s4n.capacitaciones.sesion2.ErrorHandling.{ Agua, CafeGrano, Ingrediente, Leche }
 
 /**
  * Created by daniel on 6/03/17.
  */
 case class LeerArchivoCafe() {
 
-  private val archivo: String = "/inventarioCafe.txt"
+  private val archivoCafe: String = "/inventarioCafe.txt"
+  private val archivoAgua: String = "/inventarioAgua.txt"
+  private val archivoSalidaCafe : String = "./src/main/resources/inventarioCafe2.txt"
+  private val archivoSalidaAgua : String = "./src/main/resources/inventarioAgua2.txt"
 
-  private def leerArchivoIngredientes(): (String, List[Array[String]]) = {
+  private def leerArchivoIngredientes(archivo: String): (String, List[Array[String]]) = {
     val stream: InputStream = getClass.getResourceAsStream(archivo)
     val listaIngredientes: List[String] = scala.io.Source.fromInputStream(stream).getLines.toList
     val ingredientesOrdenados: List[Array[String]] = listaIngredientes.tail.map(x => x.split(","))
@@ -53,28 +56,43 @@ case class LeerArchivoCafe() {
       case CafeGrano(origen, cantidad) => editarUnIngrediente(ingrediente)
       case _ => ingrediente
     }
-
   }
 
-  def editarUnIngrediente[A <: Ingrediente](ingrediente: A): Ingrediente = {
+  private def archivoLectura[A <: Ingrediente](ingrediente: A): String = {
+    ingrediente match {
+      case CafeGrano(origen, cantidad) => archivoCafe
+      case Agua(temperatura, cantLitros) => archivoAgua
+      case _ => ""
+    }
+  }
 
-    val (titulo: String, datos: List[Array[String]]) = leerArchivoIngredientes()
+  private def archivoEscritura[A <: Ingrediente](ingrediente: A): String = {
+    ingrediente match {
+      case CafeGrano(origen, cantidad) => archivoSalidaCafe
+      case Agua(temperatura, cantLitros) => archivoSalidaAgua
+      case _ => ""
+    }
+  }
+
+  private def editarUnIngrediente[A <: Ingrediente](ingrediente: A): Ingrediente = {
+    val (titulo: String, datos: List[Array[String]]) = leerArchivoIngredientes(archivoLectura(ingrediente))
     val ingredienteList: List[Ingrediente] = datos.map(x => crearIngrediente(ingrediente, x.head, x(1), ""))
     val filtro: List[Ingrediente] = ingredienteList.filter(x => filtrar(ingrediente, x))
 
     escribirArchivoIngrediente(titulo, seEdita(ingrediente, filtro.head), ingredienteList.filterNot(x => filtrar(ingrediente, x)))
-
   }
 
-  def escribirArchivoIngrediente[A <: Ingrediente](titulo: String, ingrediente: A, lista: List[Ingrediente]): Ingrediente = {
-    escribir(titulo :: lista.map(x => x.toString) ++ List(ingrediente.toString))
-    ingrediente
+  private def escribirArchivoIngrediente[A <: Ingrediente](titulo: String, ingrediente: A, lista: List[Ingrediente]): Ingrediente = {
+    ingrediente match {
+      case CafeGrano(origen, cantidad) =>
+        escribir(titulo :: lista.map(x => x.toString) ++ List(ingrediente.toString), archivoEscritura(ingrediente))
+        ingrediente
+      case _ => ingrediente
+    }
   }
 
-  def escribir(texto: List[String]): Unit = {
-
-    val file = new File(archivo)
-    val bw = new BufferedWriter(new FileWriter("./src/main/resources/inventarioCafe2.txt"))
+  private def escribir(texto: List[String], nombreArchivo: String): Unit = {
+    val bw = new BufferedWriter(new FileWriter(nombreArchivo))
     texto.foreach(x => bw.write(x + "\n"))
     bw.close()
     println("Escribio en el archivo")
